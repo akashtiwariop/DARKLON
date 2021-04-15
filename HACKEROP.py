332c0e8e98d8d327eab534caf9d74b908cb00f7e
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-15 09:06:05 +0530

    Update and rename Devil.py to HACKEROP.py

--- HACKEROP.py
+++ HACKEROP.py
@@ -25,16 +25,16 @@ import time
 from userbot import botnickname, ALIVE_NAME, bot
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
 xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
-pbot = Client("DEVIL", api_id=API_ID, api_hash=API_HASH, bot_token=token)
+pbot = Client("HACKER", api_id=API_ID, api_hash=API_HASH, bot_token=token)
 BOT = str(botnickname) if botnickname else "DEVIL BOT"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "DEVIL BOY"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-DEVIL = "[DEVIL](https://t.me/LUCIFEERMORNINGSTAR)"
-VERSION = "3.1.5"
-REPO = "[DEVIL BOT](https://github.com/LUCIFEERMORNINGSTAR/DEVIL)"
+HACKER = "[HACKER](https://t.me/LUCIFEERMORNINGSTAR)"
+VERSION = "0.0.1"
+REPO = "[DEVIL BOT](https://github.com/HACKERBOTTELEGRAM/HACKERBOTOP/)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
-GROUP = "[SUPPORT GROUP](https://t.me/deviluserbot)"
+GROUP = "[SUPPORT GROUP](https://t.me/hackeropuserbot)"
 if __name__=="__main__":
   xbot.run_until_disconnected()
 
