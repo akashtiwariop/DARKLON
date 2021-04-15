85561eb0b418c07038a087e27ebfcc7be08b5b24
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-15 14:02:49 +0530

    Update HACKEROP.py

--- HACKEROP.py
+++ HACKEROP.py
@@ -16,8 +16,8 @@ except:
   from pyrogram import Client, idle
 
 import asyncio
-from userbot.utils import admin_cmd as devil
-from userbot import bot as DEVILBOT
+from userbot.utils import admin_cmd as DARKLON
+from userbot import bot as DARKLONBOT
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
@@ -26,12 +26,12 @@ from userbot import botnickname, ALIVE_NAME, bot
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
 xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
 pbot = Client("HACKER", api_id=API_ID, api_hash=API_HASH, bot_token=token)
-BOT = str(botnickname) if botnickname else "DEVIL BOT"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "DEVIL BOY"
+BOT = str(botnickname) if botnickname else "DARKLON BOT"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DARKLON USER "
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-HACKER = "[HACKER](https://t.me/LUCIFEERMORNINGSTAR)"
+DARKLON = "[DARKLON](https://t.me/DARKLONUSERBOTOWNER)"
 VERSION = "0.0.1"
-REPO = "[DEVIL BOT](https://github.com/HACKERBOTTELEGRAM/HACKERBOTOP/)"
+REPO = "[DARKLON](https://github.com/HACKERBOTTELEGRAM/HACKERBOTOP/)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
 GROUP = "[SUPPORT GROUP](https://t.me/hackeropuserbot)"
